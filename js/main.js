1038d8c8f3a66d649e761b131b922be92230250d
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-19 02:15:22 +0100

    feat: change the backgroung-image position on desktop in not-homepage html files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const activePage = location.pathname;
+
+console.log(activePage);
+console.log(screen.width);
+
+if (activePage !== '/' && activePage !== '/index.html') {
+  if (screen.width <= 700) {
+    // document.body.style.backgroundPosition = 'top -620px right -400px';
+    // document.body.style.backgroundPosition = 'top -820px right -400px';
+  } else if (screen.width > 700 && screen.width < 1024) {
+    // document.body.style.backgroundPosition = 'top -700px right -750px';
+    // console.log(screen.width);
+  } else {
+    document.body.style.backgroundPosition = 'top -520px right -150px';
+  }
+}
